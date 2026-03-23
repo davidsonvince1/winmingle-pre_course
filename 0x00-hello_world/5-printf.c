2acf37f655e942da53bfe369bc54a003e34466dc
davidson <davidsonv003@gmail.com>
2026-03-23 13:05:24 +0100

    Add C hello world tasks

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+/* Write a C program that prints exactly: with proper grammar, but the outcome is a piece of art */
+int main(void){
+    printf ("this is how to add a new line\n");
+    return (0);
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*size of a chart */
+int main(void) {
+    printf("Size of a char: %zu byte(s)\n", sizeof(char));
+    printf("size of an int: %zu byte(s)\n", sizeof(int));
+    printf("size of a long int: %zu byte(s)\n", sizeof(long int));
+    printf("sise of a long long int: %zu byte(s)\n", sizeof(long long int));
+    printf("size of a float: %zu byte(s)\n", sizeof(float)); 
+    return(0);
+}
